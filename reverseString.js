42d1bb2f9172711e2bf1a6dd617830eef5be0935
Bipin <bpngaire@gmail.com>
2020-05-22 12:58:14 +0545

    reversed string without inbuilt method

--- reverseString.js
+++ reverseString.js
@@ -1,24 +1,26 @@
 var string = "my name is bipin gaire"
 console.log(string);
-var length = string.length - 1;
-var newString = new Array;
-var newString1 = new Array;
+function reverse(string) {
+    var length = string.length - 1;
+    var newString = new Array;
+    var newString1 = new Array;
 
-var j = 0;
-for (let i = length; i >= 0; i--) {
-    newString[i] = string[j]
-    j++;
+    var j = 0;
+    for (let i = length; i >= 0; i--) {
+        newString[i] = string[j]
+        j++;
+    }
+    var joined = newString.join('')
+    var array = joined.split(' ');
+    var k = 0;
+    for (let i = array.length - 1; i >= 0; i--) {
+        newString1[k] = array[i];
+        k++;
+    }
+    var joined = newString1.join(' ')
+    return joined;
 }
-var joined = newString.join('')
-var array = joined.split(' ');
-var k = 0;
-for (let i = array.length - 1; i >= 0; i--) {
-    newString1[k] = array[i];
-    k++;
-}
-var joined = newString1.join(' ')
-console.log(joined);
-
+console.log(reverse(string));
 
 
 
