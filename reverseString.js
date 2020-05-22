30b39a52d0caae25c4256453825bc9b6a1ffda6a
Bipin <bpngaire@gmail.com>
2020-05-22 10:04:29 +0545

    without inbuilt function

--- reverseString.js
+++ reverseString.js
@@ -1,9 +1,39 @@
-var str = "nipib si eman ym";
-console.log(str)
-var arr = str.split('');
-var reversedStr = arr.reverse();
-var joined = reversedStr.join("");
-var newArr = joined.split(' ');
-newArr1 = newArr.reverse();
-console.log(newArr.join(" "))
+var string = "my name is bipin gaire"
+console.log(string);
+var length = string.length - 1;
+var newString = new Array;
+var newString1 = new Array;
+
+var j = 0;
+for (let i = length; i >= 0; i--) {
+    newString[i] = string[j]
+    j++;
+}
+var joined = newString.join('')
+var array = joined.split(' ');
+var k = 0;
+for (let i = array.length - 1; i >= 0; i--) {
+    newString1[k] = array[i];
+    k++;
+}
+var joined = newString1.join(' ')
+console.log(joined);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
